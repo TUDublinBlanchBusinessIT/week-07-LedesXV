67f4987a162b4306bb7a61bc2d67965b73827acd
LedesXV <b00164804@mytudublin.ie>
2026-04-12 18:47:42 +0100

    9. Roles and Permissions - Step 5 - Enforcing the Permissions

--- resources/views/users/table.blade.php
+++ resources/views/users/table.blade.php
@@ -1,25 +1,21 @@
-<table class="table table-responsive" id="users-table">
-    <thead>
-        <th>Name</th>
-        <th>Guard Name</th>
-        <th colspan="3">Action</th>
-    </thead>
-    <tbody>
-    @foreach($users as $user)
-        <tr>
-            <td>{!! $user->name !!}</td>
-            <td>{!! $user->guard_name !!}</td>
-            <td>
-                {!! Form::open(['route' => ['users.destroy', $user->id], 'method' => 'delete']) !!}
-                <div class='btn-group'>
-                    <a href="{!! route('users.show', [$user->id]) !!}" class='btn btn-default btn-xs'><i class="far fa-eye"></i></i></a>
-                    <a href="{!! route('users.edit', [$user->id]) !!}" class='btn btn-default btn-xs'><i class="far fa-edit"></i></i></a>
-                    {!! Form::button('<i class="far fa-trash-alt"></i></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
-                    <a href="{{ route('users.assignroles', [$users->id]) }}"class='btn btn-default btn-xs'><i class="far fa-user-tag"></i></a>
-                </div>
-                {!! Form::close() !!}
-            </td>
-        </tr>
-    @endforeach
-    </tbody>
-</table>
+@foreach($users as $user)
+    <tr>
+        <td>{{ $user->name }}</td>
+        <td>{{ $user->email }}</td>
+        <td width="120">
+            <div class='btn-group'>
+                <a href="{{ route('users.show', [$user->id]) }}" class='btn btn-default btn-xs'>
+                    <i class="far fa-eye"></i>
+                </a>
+
+                <a href="{{ route('users.assignroles', [$user->id]) }}" class='btn btn-default btn-xs'>
+                    <i class="far fa-user-tag"></i>
+                </a>
+
+                <a href="{{ route('users.edit', [$user->id]) }}" class='btn btn-default btn-xs'>
+                    <i class="far fa-edit"></i>
+                </a>
+            </div>
+        </td>
+    </tr>
+@endforeach
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\memberController;
+use App\Http\Controllers\courtController;
+use App\Http\Controllers\bookingController;
+use App\Http\Controllers\userController;
+use App\Http\Controllers\rolesController;
+use App\Http\Controllers\permissionsController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -23,30 +18,42 @@ Route::get('/dashboard', function () {
 
 require __DIR__.'/auth.php';
 
-Route::resource('members', App\Http\Controllers\memberController::class);
+Route::resource('members', memberController::class)->except(['create', 'store', 'destroy']);
+Route::resource('courts', courtController::class);
+Route::resource('bookings', bookingController::class)->except(['destroy']);
 
+Route::get('/loggedInMember', [memberController::class, 'getLoggedInMemberDetails']);
 
-Route::resource('courts', App\Http\Controllers\courtController::class);
+Route::get('/users/assignroles/{id}', [userController::class, 'assignRoles'])
+    ->name('users.assignroles');
 
+Route::patch('/users/updateroles/{id}', [userController::class, 'updateRoles'])
+    ->name('roles.rolesupdate');
 
-Route::resource('bookings', App\Http\Controllers\bookingController::class);
-
-Route::get('/loggedInMember','App\Http\Controllers\memberController@getLoggedInMemberDetails');
-
-Route::resource('users', App\Http\Controllers\userController::class);
-
-
-Route::resource('roles', App\Http\Controllers\rolesController::class);
-
-Route::get('/users/assignroles/{id}','App\Http\Controllers\UserController@assignRoles')->name('users.assignroles');
-Route::patch('/users/updateroles/{id}','App\Http\Controllers\UserController@updateRoles')->name("roles.rolesupdate");
-
-Route::resource('permissions', App\Http\Controllers\permissionsController::class);
-
-Route::get('/roles/assignpermissions/{id}',
-    'App\Http\Controllers\RolesController@assignPermissions')
+Route::get('/roles/assignpermissions/{id}', [rolesController::class, 'assignPermissions'])
     ->name('roles.assignpermissions');
 
-Route::patch('/roles/updatepermissions/{id}',
-    'App\Http\Controllers\RolesController@updatePermissions')
+Route::patch('/roles/updatepermissions/{id}', [rolesController::class, 'updatePermissions'])
     ->name('roles.permissionsupdate');
+
+Route::delete('/bookings/{booking}', [bookingController::class, 'destroy'])
+    ->name('bookings.destroy')
+    ->middleware('permission:Delete Booking');
+
+Route::delete('/members/{member}', [memberController::class, 'destroy'])
+    ->name('members.destroy')
+    ->middleware('permission:Delete Member');
+
+Route::group(['middleware' => ['permission:Create New Member']], function () {
+    Route::get('/members/create', [memberController::class, 'create'])
+        ->name('members.create');
+
+    Route::post('/members/store', [memberController::class, 'store'])
+        ->name('members.store');
+});
+
+Route::group(['middleware' => ['role:System Admin']], function () {
+    Route::resource('roles', rolesController::class);
+    Route::resource('permissions', permissionsController::class);
+    Route::resource('users', userController::class);
+});
